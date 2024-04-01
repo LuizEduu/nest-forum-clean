06f93cfbaf21a03d225c759e6bcb297e3425cf99
Luiz <l.eduardo8316@gmail.com>
2024-04-01 20:13:53 -0300

    feat: create user

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { PrismaService } from './prima/prisma.service'
 import { envSchema } from './env'
+import { AuthModule } from './auth/auth.module'
 
 @Module({
   // importar outros modulos
@@ -10,6 +11,7 @@ import { envSchema } from './env'
       validate: (env) => envSchema.parse(env), // faz a validação das envs e retorna um erro caso esteja faltando algo
       isGlobal: true, // tornar o módulo global para não precisaar configurar nos outros modules
     }),
+    AuthModule,
   ],
   controllers: [],
   providers: [PrismaService],
--- src/controllers/create-account.controller.ts
+++ src/controllers/create-account.controller.ts
@@ -1,5 +1,13 @@
-import { Body, Controller, Post, UsePipes } from '@nestjs/common'
+import {
+  Body,
+  ConflictException,
+  Controller,
+  Post,
+  UsePipes,
+} from '@nestjs/common'
+import { hash } from 'bcryptjs'
 import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
+import { PrismaService } from 'src/prima/prisma.service'
 import { z } from 'zod'
 
 const createAccountBodySchema = z.object({
@@ -12,9 +20,30 @@ type CreateAccountBodySchema = z.infer<typeof createAccountBodySchema>
 
 @Controller('/accounts')
 export class CreateAccountController {
+  constructor(private readonly prisma: PrismaService) {}
+
   @UsePipes(new ZodValidationPipe(createAccountBodySchema))
   @Post()
   async handle(@Body() body: CreateAccountBodySchema) {
     const { name, email, password } = body
+
+    const userWithSameEmail = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    })
+    if (userWithSameEmail) {
+      throw new ConflictException(
+        'User with same e-mail address already exists.',
+      )
+    }
+    const hashedPassword = await hash(password, 8)
+    await this.prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      },
+    })
   }
 }
